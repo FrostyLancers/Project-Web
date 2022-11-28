75470910dc6ab50322c55249a7d614a77aaacb76
DESKTOP-KC8155E\Lenovo <EasterEggs1230@gmail.com>
2022-11-28 23:24:18 +0700

    Done Shopping Page

--- Javascript/Shopping_Page.js
+++ Javascript/Shopping_Page.js
@@ -10,13 +10,108 @@ function pageLoad(){
         appendData(data)
     })
 
+    for (var j = 0; j < 9; j++){
+        document.getElementById("Amount_in_Cart" + j).value = 0;
+    }
 }
 
-var textt = document.getElementById("Total_Text").innerHTML;
-console.log(textt);
+function Clear_Cart(){
+    for (var k = 0; k < 9; k++){
+        document.getElementById("Amount_in_Cart" + k).value = 0;
+    }
+    Calculate_Price();
+}
 
+function Checkout(){
+    var Paid = document.getElementById("Cart_Total_Number").innerHTML;
+    alert("You paid " + Paid + " Baht. Thank you for shopping.")
+}
+
+function Calculate_Price(){
+
+    var pp = document.querySelectorAll(".Price_of_Product");
+
+    for(var i = 0; i < pp.length; i++){
+        var priceText = document.getElementById("Price_Text" + i).innerHTML;
+        // var Price_in_Number = parseInt(priceText);
+        // console.log(priceText);
+
+        var amountText = document.getElementById("Amount_in_Cart" + i).value;
+        // console.log(amountText);
+
+        if(amountText < 0){
+            document.getElementById("Amount_in_Cart" + i).value = 0;
+            amountText = 0;
+        }
+
+
+        // var pp = document.querySelectorAll(".Price_of_Product");
+        // // console.log(pp);
+        // // console.log(pp[i].innerText);
+
+        // var ppp = pp[i].innerText;
+        // // console.log(ppp[0],ppp[1], ppp[2],ppp[3]);
+
+        // var Price_in_Number = parseInt(ppp[0].concat(ppp[1]));
+        // console.log(Price_in_Number);
+
+
+        var total = parseInt(priceText) * parseInt(amountText);
+
+        document.getElementById("Total_Text" + i).innerHTML = total;
+
+        totalText = total;
+        console.log(totalText);
+    }
+
+    var CartSubNumber = document.getElementById("Cart_Subtotal_Number").innerHTML;
+    CartSubNumber = 0;
+    
+    for (var k = 0; k < 9; k++){
+        CartSubNumber = CartSubNumber + parseInt(document.getElementById("Total_Text" + k).innerHTML);
+    }
+
+    document.getElementById("Cart_Subtotal_Number").innerHTML = CartSubNumber;
+    document.getElementById("Cart_Tax_Number").innerHTML = Math.round(CartSubNumber * (0.07));
+    document.getElementById("Cart_Total_Number").innerHTML = CartSubNumber + parseFloat(document.getElementById("Cart_Tax_Number").innerHTML);
 
     
+    // console.log(CartSubNumber);
+
+
+
+    // var priceText = document.getElementById("Price_Text").innerHTML;
+    // console.log(priceText);
+
+    // var pp = document.querySelectorAll(".Price_of_Product");
+    // console.log(pp);
+    // console.log(pp[1].innerText);
+
+    // var ppp = pp[0].innerText;
+    // console.log(ppp[0],ppp[1], ppp[2],ppp[3]);
+
+    // var pppp = parseInt(ppp[0].concat(ppp[1]));
+    // console.log(pppp);
+
+
+
+
+
+
+    // var amountText = document.getElementById("Amount_in_Cart").value;
+    // console.log(amountText);
+
+    // var total = parseInt(priceText) * parseInt(amountText);
+
+    // document.getElementById("Total_Text").innerHTML = total;
+
+    // totalText = total;
+    // console.log(totalText);
+
+}
+    
+
+
 function appendData(data){
     
     for(var i = 0; i < data.length; i++){
@@ -31,7 +126,6 @@ function appendData(data){
         container.appendChild(img);
         container.appendChild(text);
     }
-
     
 
     // for(var i = 0; i < data.length; i++){
